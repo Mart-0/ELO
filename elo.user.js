5e10f6f46b50ec5480da88e9a3d0706e65a26cfd
Mart <you@example.com>
2022-04-19 20:57:41 +0200

    1.4.4 add course

--- elo.user.js
+++ elo.user.js
@@ -8,7 +8,7 @@
 // @downloadURL   https://github.com/Mart-0/ELO/raw/master/elo.user.js
 // @updateURL     https://github.com/Mart-0/ELO/raw/master/elo.user.js
 // @supportURL    https://github.com/Mart-0/ELO/issues
-// @version       1.4.3
+// @version       1.4.4
 
 // @match         https://elo.windesheim.nl/*
 // @grant         none
@@ -745,6 +745,7 @@ const abbreviations = {
   78802: 'SEC',
   58556: 'BE',
   78801: 'ST',
+  80973: 'CI',
 }
 
 // icon list for the difrent file types
